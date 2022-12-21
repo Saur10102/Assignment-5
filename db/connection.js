70a4ad5cec2a081d07bd65f2c915674008dc9e67
Saur10102 <116869247+Saur10102@users.noreply.github.com>
2022-12-21 16:54:51 +0530

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose')
-mongoose.connect("mongodb+srv://Saurabhkute56:<password>@cluster0.ltz0e7p.mongodb.net/test?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://Saurabhkute56:saurabh@cluster0.ltz0e7p.mongodb.net/test")
     .then(res => console.log("Database Connected"))
     .catch(err => console.log("Error : " + err));
 //end
